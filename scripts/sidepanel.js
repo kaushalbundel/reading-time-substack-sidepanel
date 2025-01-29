866b68030a68ba43de3bb67a9130f8a26065a1af
Kaushal Bundel <kaushalbundel@gmail.com>
2025-01-29 19:40:08 +0530

    sidepanel updated to accommodate tab changes
    - On the cases where the tab changes (multiple substack tabs)

--- scripts/sidepanel.js
+++ scripts/sidepanel.js
@@ -1,28 +1,48 @@
 //storing the current tab url
-let currentUrl = "";
+let currentTabId = "";
 
 //initialize notes from chrome storage as the panel opens
 document.addEventListener("DOMContentLoaded", async () => {
-  // get the current tab url
-  const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
-  currentUrl = tabs[0].url;
+  try {
+    // get the current tab url
+    const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+    if (tabs && tabs[0]) {
+      currentTabId = tabs[0].id;
+      currentUrl = tabs[0].url;
 
-  //load saved notes for this url
-  const results = await chrome.storage.local.get(currentUrl);
-  const savedNotes = results[currentUrl] || "";
-  document.getElementById("notes-textarea").value = savedNotes;
+      //load saved notes for this url
+      const results = await chrome.storage.local.get(currentUrl);
+      const savedNotes = results[currentUrl] || "";
+      document.getElementById("notes-textarea").value = savedNotes;
+    }
+  } catch (error) {
+    console.error("Error initializing sidepanel:", error);
+  }
+});
+
+// listen for tab changes
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+  currentTabId = activeInfo.TabId;
 });
 
 // listen to message from the background scripts and shows it on the side panel
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  console.log("side panel recieved message", message);
-  if (message.type === "READING_TIME") {
-    const timeElement = document.getElementById("reading-time");
-    if (timeElement) {
-      timeElement.textContent = `Reading time: ${message.time} mins`;
-    } else {
-      console.error("reading time not found in the side panel");
+  try {
+    console.log("side panel recieved message", message);
+    if (
+      message.type === "READING_TIME" &&
+      message.source === "background" &&
+      message.tabId === currentTabId
+    ) {
+      const timeElement = document.getElementById("reading-time");
+      if (timeElement) {
+        timeElement.textContent = `Reading time: ${message.time} mins`;
+      } else {
+        console.error("reading time not found in the side panel");
+      }
     }
+  } catch (error) {
+    console.error("error processing message", error);
   }
 });
 
